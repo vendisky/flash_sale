83b9c618be894cf3061d3a5c06356da421862af7
Tianze Wang <vendisky@163.com>
2022-04-12 12:09:46 +0800

    Init project

---
+++ src/main/java/com/imooc/flashsale/ImoocFlashSaleApplication.java
@@ -0,0 +1,13 @@
+package com.imooc.flashsale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImoocFlashSaleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImoocFlashSaleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/imooc/flashsale/ImoocFlashSaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imooc.flashsale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImoocFlashSaleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
